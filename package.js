0d250d8f8852ba2042f1b8ed39693e9cc89b8f58
Tom Coleman <tom@thesnail.org>
2013-01-25 13:21:41 +1100

    Basic linked package.

---
+++ package.js
@@ -0,0 +1,7 @@
+Package.describe({
+  summary: "HTML5 History API expansion for browsers not supporting pushState, replaceState"
+});
+
+Package.on_use(function (api, where) {
+  api.add_files('HTML5-History-API/history.iegte8.js', 'client');
+});
